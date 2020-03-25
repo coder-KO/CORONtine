ea5ecc5f08b45d29be0200ce0d97150d6677fd27
Krishna Ojha <drkaojha@gmail.com>
2020-03-25 14:35:29 +0530

    Ultrasonic_Configure updated

---
+++ hardware/configuration_codes/arduino_to_python.py
@@ -0,0 +1,24 @@
+import serial
+arduino = serial.Serial('COM9', 9600)
+
+rawdata=[]
+count=0
+while count<=10: 
+    rawdata.append(str(arduino.readline()))
+    count+=1
+print(rawdata)
+def clean(L):
+    newl=[]
+    for i in range(len(L)):
+        temp=L[i][2:]
+        newl.append(temp[:-5])
+    return newl
+cleandata=clean(rawdata)
+
+def write(L):
+    file=open("data.txt",mode='w')
+    for i in range(len(L)):
+        file.write(L[i]+'\n')
+    file.close()
+
+write(cleandata)
