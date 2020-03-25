4e9e9a09a8fcf95de93d4cb452877e25ba100fb1
Krishna Ojha <44628947+coder-KO@users.noreply.github.com>
2020-03-25 14:36:57 +0530

    Ultrasonic_Configure updated

--- hardware/configuration_codes/Ultrasonic_Configure/Ultrasonic_Configure.ino
+++ hardware/configuration_codes/Ultrasonic_Configure/Ultrasonic_Configure.ino
@@ -1,42 +1,38 @@
 
 // For testing the Ultrasonic Sensor
+// This code also sends data to python shell for testing
 
-#define trigPin 11
-#define echoPin 12
-#define LEDPin 13 
-
-int maximumRange = 200; 
-int minimumRange = 0; 
-long duration, distance; 
-
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+long duration, cm, inches;
+ 
 void setup() {
- Serial.begin (9600);
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
- pinMode(LEDPin, OUTPUT); 
+  Serial.begin (9600);
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
-
+ 
 void loop() {
- digitalWrite(trigPin, LOW); 
- delayMicroseconds(2); 
-
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
  
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
  
- //Calculate the distance (in cm) based on the speed of sound.
- distance = duration/58.2;
- 
- if (distance >= maximumRange || distance <= minimumRange){
- Serial.println("Out of Range!");
- digitalWrite(LEDPin, HIGH); 
- }
- else {
- Serial.println(distance);
- digitalWrite(LEDPin, LOW); 
- }
- 
- delay(50);
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  Serial.flush();
+  
+  delay(250);
 }
