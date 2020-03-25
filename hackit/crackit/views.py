19b6ca0806b635835927a42277d7ed1b31fa71b0
ektaarora3501 <ektaarora3501@gmail.com>
2020-03-26 02:27:20 +0530

    integrating arduino script

--- hackit/crackit/views.py
+++ hackit/crackit/views.py
@@ -9,35 +9,77 @@ from django.utils.translation import ugettext_lazy as _
 import os
 import serial
 from gtts import gTTS
-# from video_image import front
-#import pyrebase
+import pygame
+import time
+import random
+from video_image import front
+from test import prediction
+# arduino = serial.Serial('COM9', 9600)
 
-#
 
 
 def Index(request):
-    # arduino = serial.Serial('COM9', 9600)
-    #
-    # global flag_dis,flag_angle
-    #
-    # while(1):
-    #     data = str(arduino.readline()) #reading from arduino serial monitor
-    #     #assuming distance
-    #     if(data<=1.5):
-    #         #desired distance
-    #         flag_dis=1
-    #         return redirect()
-    #
-    #     else:
-    #         text = "move closer"
-    #         lan='en'
-    #
-    #         myobj = gTTS(text=text,lang=lan,slow=False)
-    #         myobj.save("hello.mp3")
-    #         os.system("mpg321 hello.mp3")
-    #
+
+    global flag_dis
+
+    while 1:
+        flag_dis = 0;
+        # reading from arduino serial monitor
+        # data = int(arduino.readline())  # acknowledgement flag
+        data = 1111
+        print(data)
+
+        if data != 0000:
+            # Checking person's position
+            if data == 1111:
+                text = "please move closer by"
+            elif data == 2222:
+                text = "please move back by"
+            else:
+                break
+            lan = 'en'
+            # flag_dis=1
+            # data2 = int(arduino.readline()) # distance
+            data2=6
+            print(data2)
+            text += str(data2)
+            text += "cm"
+        else:
+            text = "scan starting, please stay still"
+            flag_dis = 1
+
+        lan = 'en'
+        myobj = gTTS(text=text, lang=lan, slow=False)
+        myobj.save("guide.mp3")
+        pygame.mixer.init()
+        pygame.mixer.music.load("guide.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(20)
+            #pygame.time.Clock().tick(500)
+        pygame.mixer.music.load("beep.mp3")
+        os.remove("guide.mp3")
+
+        # block of code if person is correctly positioned
+        if flag_dis == 1:
+            # while 1:
+            #     time.sleep(10)
+            #     print('.')
+
+            return HttpResponseRedirect(reverse('take_images'))
+
+
+
     return render(request,'index.html')
 
 
 def take_images(request):
-      front(str(id))
+      v=str(random.randrange(1000,9999))
+
+      front(str(v))
+      count = prediction(str(v))
+
+      if(count>=2):
+          return render(request,"warning.html")
+
+      return render(request,"index.html")
