13d7a74450cb0503de0a0f5207be66c58a05f7cb
ektaarora3501 <ektaarora3501@gmail.com>
2020-04-08 21:38:48 +0530

    final merge

--- hackit/crackit/urls.py
+++ hackit/crackit/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
    path('',views.Index,name='index_page'),
    path('image',views.take_images,name="images"),
    path('detect',views.Detect,name="detection"),
+   path('chest_scan',views.chest_scan,name='chest_scan'),
    # path('random',views.random,name='random'),
 
 ]
