3a07aaf0566a89b6f43c8f4bf0ee320150a8efb6
ektaarora3501 <ektaarora3501@gmail.com>
2020-04-08 21:33:03 +0530

    Modyfying test images

--- hackit/test.py
+++ hackit/test.py
@@ -1,60 +1,47 @@
 from os import listdir
-from os.path import isfile,join
-import numpy as np
-import pandas as pd
-from keras.models import load_model
-from keras.optimizers import Adam
+from os.path import join,isfile
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
-import csv
+from keras.models import load_model
+import time
+import tensorflow as tf
 from keras import backend as K
 import tensorflow as tf
-K.clear_session()
+import numpy as np
+# K.clear_session()
 
+model = load_model("model1_6_15.h5")
+# model._make_predict_function()
 graph1 = tf.get_default_graph()
 
-with tf.Session() as sess:
-      sess.run(tf.global_variables_initializer())
+# with tf.Session() as sess:
+#       sess.run(tf.global_variables_initializer())
 
 
-model = load_model('model5_3_10.h5')
-model.compile(loss='mean_squared_error',
-               optimizer='Adam',
-               metrics=['accuracy'])
 
-# this is tesr
 
-def prediction(id):
+test_dir = '/home/ekta3501/opensource/Dev.ino_HackCovid19/chest/test/'
 
-    test_dir = 'F:\Github\HackCovid\Dev.ino_HackCovid19\hackit\images'+id+'/'
+model.compile(loss="categorical_crossentropy",
+            optimizer='adam',
+            metrics=['accuracy']
+)
 
+def test_chest():
     onlyfiles = [f for f in listdir(test_dir) if isfile(join(test_dir, f))]
-    #
-    # print(onlyfiles)
-    #
+    print(onlyfiles)
 
-    count = 0
     for files in onlyfiles:
-        img = image.load_img(test_dir+files,target_size=(48,48))
-        # print(img.shape)
-        x = image.img_to_array(img)
-        x = np.expand_dims(x,axis=0)
+       img = image.load_img(test_dir+files,target_size=(200,200))
+       x = image.img_to_array(img)
+       x = np.expand_dims(x,axis=0)
 
-        images = np.vstack([x])
-        # plt.imshow(images)
-        with graph1.as_default():
-            classes = model.predict_classes(images,batch_size=3)
+       images = np.vstack([x])
+       # plt.imshow(images)
+       with graph1.as_default():
+           classes = model.predict_classes(images,batch_size=3)
+       print(classes,files)
+       classes = classes[0]
 
+       return classes
 
-        # print(classes[0])
-        arr = ['cough','healthy']
-
-        print(files,classes)
-
-
-        classes = classes[0]
-        if(classes==0):
-            count+=1
-
-
-    return count
+# {'covid_xrays': 0, 'non_covid_xrays': 1}
