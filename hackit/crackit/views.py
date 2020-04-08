7fc1163caf6a47139537a5dc3f29934fef02b243
ektaarora3501 <ektaarora3501@gmail.com>
2020-04-08 20:50:56 +0530

    merge

--- hackit/crackit/views.py
+++ hackit/crackit/views.py
@@ -12,9 +12,15 @@ from gtts import gTTS
 import pygame
 import time
 import random
-from video_image import front
-from test import prediction
+# from video_image import front
+from live_test import predicting_cough
 # arduino = serial.Serial('COM9', 9600)
+from audio_positioning import arduino_call
+from test import test_chest
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 
@@ -25,89 +31,38 @@ def Index(request):
 
 def Detect(request):
     global flag_dis
+    arduino_call()
 
-    while 1:
-        flag_dis = 0;
-        # reading from arduino serial monitor
-
-        # data = int(arduino.readline())  # acknowledgement flag
-        # data = 1111
-        # data = int(arduino.readline())  # acknowledgement flag
-        data = 1111
-        print(data)
-
-        if data != 0000:
-            # Checking person's position
-            if data == 1111:
-                text = "please move closer by"
-            elif data == 2222:
-                text = "please move back by"
-            else:
-                break
-            lan = 'en'
-            flag_dis=1
-            # data2 = int(arduino.readline()) # distance
-            data2=6
-            print(data2)
-            text += str(data2)
-            text += "cm"
-        else:
-            text = "scan starting, please stay still"
-            flag_dis = 1
-
-        # lan = 'en'
-        # myobj = gTTS(text=text, lang=lan, slow=False)
-        # myobj.save("guide.mp3")
-        # pygame.mixer.init()
-        # pygame.mixer.music.load("guide.mp3")
-        # pygame.mixer.music.play()
-        # while pygame.mixer.music.get_busy():
-        #     time.sleep(20)
-        #     #pygame.time.Clock().tick(500)
-        # pygame.mixer.music.load("beep.mp3")
-        # os.remove("guide.mp3")
-
-        # block of code if person is correctly positioned
-        if flag_dis == 1:
-            # while 1:
-            #     time.sleep(10)
-            #     print('.')
-
-            return HttpResponseRedirect(reverse('images'))
+    return HttpResponseRedirect(reverse('images'))
 
 
-    return render(request,'index.html')
+    # return render(request,'index.html')
 
 
 
 
 def take_images(request):
       v=str(random.randrange(1000,9999))
-      data = 0
-      front(str(v))
-      count = prediction(str(v))
-    #   count=2
-      if(count>=2):
-          text = "You need to be quarantined"
-          lan = 'en'
-          myobj = gTTS(text=text, lang=lan, slow=False)
-          data = 1
-          # pygame.mixer.init()
-          # pygame.mixer.music.load("guide.mp3")
-          # pygame.mixer.music.play()
-          # while pygame.mixer.music.get_busy():
-          #     time.sleep(20)
-          #     # pygame.time.Clock().tick(500)
-          # pygame.mixer.music.load("beep.mp3")
-          # os.remove("guide.mp3")
-          # return render(request,"warning.html")
+      data = predicting_cough()
 
       return render(request,"index.html",{'data':data})
 
 
 
-# def random(request):
-#     context={
-#     'data':1,
-#     }
-#     return render(request,'random.html',context)
+def chest_scan(request):
+    data = test_chest()  #if data == 1 => non covid person
+
+    # img = cv2.imread('/home/ekta3501/opensource/Dev.ino_HackCovid19/chest/test/covid-19-caso-70-1-PA.jpg',
+    # 				cv2.IMREAD_GRAYSCALE)
+    #
+    # img  = cv2.resize(img,(700,900))
+    #
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+    # cv2.destoryAllWindows()
+
+    if data:
+        val = 0
+    else:
+        val = 1  # => patient has covid 19
+    return render(request,"index.html",{"val":val})
